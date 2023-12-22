315a1d5fde2b67f8a9944eb5070cd54e1a04daec
raiyan000 <raiyanar0@gmail.com>
2023-12-22 17:28:49 +0530

    rds

--- today/settings.py
+++ today/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'today.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'today',
+        'USER': 'admin',
+        'PASSWORD': '12345678',
+        'HOST': 'database-1.c1i0qm828esd.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
